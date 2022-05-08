1d2eece30cf5c637a80930deaf788966d7fc742a
wsy <2161826815@qq.com>
2022-05-08 10:47:31 +0800

    Delete 13_misc directory

--- 13_misc/.driver.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wsy/driver/13_misc/driver.mod := { echo  /home/wsy/driver/13_misc/driver.o; echo; } > /home/wsy/driver/13_misc/driver.mod
--- 13_misc/driver.mod
+++
@@ -1,2 +0,0 @@
-/home/wsy/driver/13_misc/driver.o
-
