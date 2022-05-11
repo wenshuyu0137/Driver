26ee363e72368067e8ae1642d77f4ee5d015bdef
wsy <2161826815@qq.com>
2022-05-11 17:04:16 +0800

    Update AP3216C_reg.h

--- 15_i2c/AP3216C_reg.h
+++ 15_i2c/AP3216C_reg.h
@@ -3,7 +3,7 @@
 
 #define System_Configuration    0x00
 #define INT_Status              0x01
-#define INT_Clear Manner        0x02
+#define INT_Clear_Manner        0x02
 #define IR_Data_Low             0x0A
 #define IR_Data_High            0x0B
 #define ALS_Data_Low            0x0C
